1b0a602ac3c111855aeec879fa7fb672c9431947
danserboi <57451294+danserboi@users.noreply.github.com>
2020-06-22 23:53:24 +0300

    Add files via upload

---
+++ include/list.h
@@ -0,0 +1,16 @@
+// SERBOI FLOREA-DAN 325CB
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct cell *list;
+
+struct cell
+{
+  void *element;
+  list next;
+};
+
+extern list cons(void *element, list l);
+extern list cdr_and_free(list l);
+
+#endif
